8d3aa023121baedc6d2ff1210b28e74de8f34071
Panduranga <panduranga1797@gmail.com>
2026-04-21 21:32:09 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Background from "./globalbackground";
+import Intro from "./intro";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Background />} />
+        <Route path="/" element={<Intro />} />
+        <Route path="/background" element={<Background />} />
         
       </Routes>
     </Router>
