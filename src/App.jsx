2f06ce85694e90ad0c394024a72925be5f697ddf
Panduranga <panduranga1797@gmail.com>
2026-04-23 16:24:44 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Background from "./globalbackground";
 import Intro from "./intro";
 import Quiz from "./quiz";
+import ResultsDashboard from "./resultsdashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Intro />} />
         <Route path="/background" element={<Background />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<ResultsDashboard />} />
       </Routes>
     </Router>
   );
